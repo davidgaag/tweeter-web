68f1f9dadf5fd5d3a6b5d566dc4753e1e1e0d9e3
David Gaag <davidgaag3@gmail.com>
2024-02-01 00:23:27 -0700

    Finish Milestone 1 Steps 1, 2 React Components

--- tweeter-web/src/App.tsx
+++ tweeter-web/src/App.tsx
@@ -12,10 +12,10 @@ import Login from "./components/authentication/login/Login";
 import Register from "./components/authentication/register/Register";
 import MainLayout from "./components/mainLayout/MainLayout";
 import Toaster from "./components/toaster/Toaster";
-import FeedScroller from "./components/mainLayout/FeedScroller";
-import StoryScroller from "./components/mainLayout/StoryScroller";
-import { AuthToken, User, FakeData } from "tweeter-shared";
+import { AuthToken, User, FakeData, Status } from "tweeter-shared";
 import UserItemScroller from "./components/mainLayout/UserItemScroller";
+import StatusItemScroller from "./components/mainLayout/StatusItemScroller";
+
 
 const App = () => {
   const { currentUser, authToken } = useContext(UserInfoContext);
@@ -59,28 +59,42 @@ const AuthenticatedRoutes = () => {
     return FakeData.instance.getPageOfUsers(lastItem, pageSize, user);
   };
 
+  const loadMoreStoryItems = async (
+    authToken: AuthToken,
+    user: User,
+    pageSize: number,
+    lastItem: Status | null
+  ): Promise<[Status[], boolean]> => {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+  };
+
+  const loadMoreFeedItems = async (
+    authToken: AuthToken,
+    user: User,
+    pageSize: number,
+    lastItem: Status | null
+  ): Promise<[Status[], boolean]> => {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+  };
+
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route index element={<Navigate to="/feed" />} />
-        <Route path="feed" element={<FeedScroller />} />
-        <Route path="story" element={<StoryScroller />} />
+        <Route path="feed" element={<StatusItemScroller loadItems={loadMoreFeedItems} statusDescription="feed" />} />
+        <Route path="story" element={<StatusItemScroller loadItems={loadMoreStoryItems} statusDescription="story" />} />
         <Route
           path="following"
           element={
-            <UserItemScroller
-              loadItems={loadMoreFollowees}
-              itemDescription="followees"
-            />
+            <UserItemScroller loadItems={loadMoreFollowees} itemDescription="followees" />
           }
         />
         <Route
           path="followers"
           element={
-            <UserItemScroller
-              loadItems={loadMoreFollowers}
-              itemDescription="followers"
-            />
+            <UserItemScroller loadItems={loadMoreFollowers} itemDescription="followers" />
           }
         />
         <Route path="logout" element={<Navigate to="/login" />} />
